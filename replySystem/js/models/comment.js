92ae02193be559a91602dc3b1f0e520e4214287f
Nikunj Goel <72417259+nikunj-oss@users.noreply.github.com>
2024-03-14 04:55:22 +0530

    update

--- replySystem/js/models/comment.js
+++
@@ -1,8 +0,0 @@
-// models/comment.js
-import mongoose from "mongoose"
-const commentSchema = new mongoose.Schema({
-    content: String,
-    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }]
-});
-
-export const Comment = mongoose.model('Comment', commentSchema);
